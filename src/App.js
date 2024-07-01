f57fda21add188159db32f8e2c7516ddc5b7b134
Yogesh Kumar S (INFOSYS LIMITED) <v-yogs@microsoft.com>
2024-07-01 19:48:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Easy to learns
+          Easy to learns okay
         
         </a>
       </header>
